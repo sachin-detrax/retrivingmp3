9a1a640805a6857072b3fba7b2eb3a4bf5746dbb
sachin-detrax <84434145+sachin-detrax@users.noreply.github.com>
2021-06-02 20:24:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/retrivingmp3/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.retrivingmp3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    public List<AudioModel> getAllAudioFromDevice( Context context) {
+
+         List<AudioModel> tempAudioList = new ArrayList<>();
+
+
+
+
+
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = {MediaStore.Audio.AudioColumns.DATA, MediaStore.Audio.AudioColumns.ALBUM, MediaStore.Audio.ArtistColumns.ARTIST,};
+        Cursor c = context.getContentResolver().query(uri,
+                projection,
+                null,
+                null,
+                null);
+
+        if (c != null) {
+            while (c.moveToNext()) {
+
+                AudioModel audioModel = new AudioModel();
+                String path = c.getString(0);
+                String album = c.getString(1);
+                String artist = c.getString(2);
+
+                String name = path.substring(path.lastIndexOf("/") + 1);
+
+                audioModel.setaName(name);
+                audioModel.setaAlbum(album);
+                audioModel.setaArtist(artist);
+                audioModel.setaPath(path);
+
+                Log.e("Name :" + name, " Album :" + album);
+                Log.e("Path :" + path, " Artist :" + artist);
+                //TODO fill these data with a view
+
+
+
+
+
+                tempAudioList.add(audioModel);
+            }
+            c.close();
+        }
+
+        return tempAudioList;
+
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        //TODO show all songs here
+                getAllAudioFromDevice(MainActivity.this);
+
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+                    }
+                }).check();
+    }
+}
